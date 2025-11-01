7ef14dc16aa708a074e1b0a8e738d2dc66f36b68
palimo <palimosrehans@gmail.com>
2025-11-01 10:33:47 +0700

    update api connect logo

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
-
+const url = {
+    local: "http://localhost:3030/api/v1",
+    global: "https://web-ticketing-back.vercel.app/api/v1"
+}
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3030/api/v1",
+    baseURL:url.global,
     // withCredentials:true,
     headers:{
         "Content-Type": "application/json",
